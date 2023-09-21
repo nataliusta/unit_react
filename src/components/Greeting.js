325533bf0ce93378e423ed106d490e3f4be5bc2f
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-21 18:42:39 +0300

    Add Async component

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import Output from './Output';
+
 const Greeting = () => {
     const [changedText, setChangedText] = useState(false);
 
@@ -10,8 +12,8 @@ const Greeting = () => {
     return (
         <div>
             <h2>Hello World!</h2>
-            {!changedText && <p>It's good to see you!</p>}
-            {changedText && <p>Changed!</p>}
+            {!changedText && <Output>It's good to see you!</Output>}
+            {changedText && <Output>Changed!</Output>}
             <button onClick={onClickHandler}>Change text!</button>
         </div>
     );
