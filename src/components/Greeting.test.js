77e9999a712aae892dd6f1254608a9a688e9c2af
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-09-21 16:57:35 +0300

    Create test for Greeting component

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import Greeting from './Greeting';
+
+test('renders Hello World as a text', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+
+    // Assert
+    const helloWorldElement = screen.getByText('Hello World!');
+    expect(helloWorldElement).toBeInTheDocument();
+});
